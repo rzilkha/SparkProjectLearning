16a9eb7793bd347976962d578d74f3d9bf83b78c
Roee Zilkha <rzilkha@gmail.com>
2017-10-21 16:26:47 +0300

    ex1

--- src/main/scala/example/TotalAmountByCustomer.scala
+++ src/main/scala/example/TotalAmountByCustomer.scala
@@ -18,12 +18,12 @@ object TotalAmountByCustomer {
     val conf = new SparkConf().setAppName("example")
     val sc = new SparkContext(conf);
     val rdd = sc.textFile("customer-orders.csv").map(parseLine)
-      .reduceByKey((price1, price2) => price1 + price2).sortByKey();
+      .reduceByKey((price1, price2) => price1 + price2).sortBy(item => item._2,ascending = true)
 
     val results = rdd.collect();
 
     for ((key, value) <- results) {
-      println(s"id:$key"+ f"amount:$value%1.2f")
+      println(s"id:$key"+ f" $value%1.2f")
     }
   }
 
